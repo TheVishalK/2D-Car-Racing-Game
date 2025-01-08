1ae75635a4be016be92c42f5f84078c7e9f9f95d
Vishal Kumar <javacheartofmine@gmail.com>
2025-01-08 22:50:57 +0530

    Draw the player car

--- game.js
+++ game.js
@@ -4,3 +4,9 @@ const ctx = canvas.getContext("2d");
 // Set the canvas size to fill the screen
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+function drawCar() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, 0, 40, 100);
+}
+drawCar();
